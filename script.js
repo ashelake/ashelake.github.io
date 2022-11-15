b6d037e7f3622fb03ca1cd0bc76e89a2e2dff3cb
ashelake <ashelake88@gmail.com>
2022-11-15 10:24:11 +0530

    git Stat added

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('active');
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("active");
 };
 
 window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("active");
 };
 
-const sr = ScrollReveal ({
-	distance: '45px',
-	duration: 2700,
-	reset: true
-})
+const sr = ScrollReveal({
+  distance: "45px",
+  duration: 2700,
+  reset: true,
+});
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal(".home-text", { delay: 350, origin: "left" });
+sr.reveal(".home-img", { delay: 350, origin: "right" });
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+sr.reveal(".sub-service,.about,.portfolio,.service,.cta,.contact", {
+  delay: 200,
+  origin: "bottom",
+});
 
 GitHubCalendar(".calendar", "ashelake");
 // or enable responsive functionality
